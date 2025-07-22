1015636d3a4fa761f1fec0f50cd06c76f6f21e0e
MisterMAD1 <mistermad.official@gmail.com>
2025-07-22 10:47:39 +0700

    lab6

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home";
+import Profile from "./Profile";
+import Posts from "./Posts";
 
-function App() {
-  return (
-    <div className="container mt-4">
-      <div className="alert alert-primary" role="alert">
-        This is a primary alert—check it out!
-      </div>
-      <div className="alert alert-secondary" role="alert">
-        This is a secondary alert—check it out!
-      </div>
-      <div className="alert alert-success" role="alert">
-        This is a success alert—check it out!
-      </div>
-      <div className="alert alert-danger" role="alert">
-        This is a danger alert—check it out!
-      </div>
-      <div className="alert alert-warning" role="alert">
-        This is a warning alert—check it out!
-      </div>
-      <div className="alert alert-info" role="alert">
-        This is an info alert—check it out!
-      </div>
-      <div className="alert alert-light" role="alert">
-        This is a light alert—check it out!
-      </div>
-      <div className="alert alert-dark" role="alert">
-        This is a dark alert—check it out!
-      </div>
-      <div className="alert bg-white text-dark border" role="alert">
-        This is a white alert—check it out!
-      </div>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
+import { BrowserRouter as Switch } from "react-router-dom";
+import "./App.css";
+const App = () => (
+  <div className="App">
+    <h1>My React Router dom. </h1>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Posts">All Posts</Link>
+        </li>
+        <li>
+          <Link to="/Profile">User Profile</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Posts" element={<Posts />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route exact path="/Home" element={<Home />} />
+      </Routes>
+    </Router>
+  </div>
+);
 export default App;
---
+++ src/Post.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Post() {
+  return (
+    <div className="App">
+      <h1>Post</h1>
+    </div>
+  );
+}
+export default Post;
