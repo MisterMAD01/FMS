60386cf77ba958e087a4d4d8ab8fa225cacb1cc2
MisterMAD1 <mistermad.official@gmail.com>
2025-07-30 16:03:53 +0700

    Lab9

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
-import Header from "./Header";
-const App = () => {
-  return <Header />;
-};
+import React, { useState, useCallback, createContext } from "react";
+import "./App.css";
+import Login from "./Login";
+import User from "./User";
+export const AppContext = createContext(null);
+function App() {
+  const [username, setUsername] = useState("");
+  return (
+    <div className="App">
+      <AppContext.Provider value={{ username, setUsername }}>
+        <Login />
+        <User />
+      </AppContext.Provider>
+    </div>
+  );
+}
 export default App;
