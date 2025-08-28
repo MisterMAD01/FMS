adfb0e1d0b1ff398b254bb8c31b3906a181090f0
MisterMAD1 <mistermad.official@gmail.com>
2025-08-28 13:51:18 +0700

    5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+// src/App.js
+
 import React from "react";
-import Header from "./Header";
-const App = () => {
-  return <Header />;
-};
+import Calculator from "./Calculator";
+import "./App.css";
+
+function App() {
+  const numberA = 100;
+  const numberB = 25;
+
+  return (
+    <div className="App">
+      <h1>เครื่องคิดเลขแบบ React Props</h1>
+      <div className="calculator-container">
+        <Calculator a={numberA} b={numberB} operation="add" />
+        <Calculator a={numberA} b={numberB} operation="subtract" />
+        <Calculator a={numberA} b={numberB} operation="multiply" />
+        <Calculator a={numberA} b={numberB} operation="divide" />
+        <Calculator a={numberA} b={numberB} operation="mod" />
+      </div>
+    </div>
+  );
+}
+
 export default App;
--- src/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./App.css";
-function Home() {
-  return <h1>My Home</h1>;
-}
-export default Home;
